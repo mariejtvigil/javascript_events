1a297a08f77b6956d782bacb8fcfe8d2f8305fe9
Marie Vigil <mjkvigil@gmail.com>
2015-08-03 13:44:35 -0600

    created javascript exercise php file

---
+++ javascript exercise.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- ////////////////////////////////////////////////
+		 //// LINK TO YOUR CUSTOM CSS FILES HERE
+		 //// <link type="text/css" href="css/style.css" rel="stylesheet"/>
+		 ///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+		<title>Playing with javaScript</title>
+
+
+	</head>
+	<body>
+		<h1>    </h1>
+		<p>"To be, or not to be..." is the opening phrase of a soliloquy in the "Nunnery Scene"[1] of William Shakespeare's play Hamlet.
+			In the speech, a despondent or feigning Prince Hamlet contemplates death and suicide. He bemoans the pains and unfairness of life but acknowledges the alternative might be still worse. The speech functions within the play to explain Hamlet's hesitation to directly and immediately avenge his father's murder (discovered in Act I) on his uncle, stepfather, and new king Claudius. Claudius and his minister Polonius[2] are preparing to eavesdrop on Hamlet's interaction with Ophelia.[3]  </p>
+		<p> To be, or not to be- that is the question:
+			Whether 'tis nobler in the mind to suffer
+			The slings and arrows of outrageous fortune
+			Or to take arms against a sea of troubles,
+			And by opposing end them. To die- to sleep-
+			No more; and by a sleep to say we end
+			The heartache, and the thousand natural shocks
+			That flesh is heir to. 'Tis a consummation
+			Devoutly to be wish'd. To die- to sleep.
+			To sleep- perchance to dream: ay, there's the rub!
+			For in that sleep of death what dreams may come
+			When we have shuffled off this mortal coil,
+			Must give us pause. There's the respect
+			That makes calamity of so long life.
+			For who would bear the whips and scorns of time,
+			Th' oppressor's wrong, the proud man's contumely,
+			The pangs of despis'd love, the law's delay,
+			The insolence of office, and the spurns
+			That patient merit of th' unworthy takes,
+			When he himself might his quietus make
+			With a bare bodkin? Who would these fardels bear,
+			To grunt and sweat under a weary life,
+			But that the dread of something after death-
+			The undiscover'd country, from whose bourn
+			No traveller returns- puzzles the will,
+			And makes us rather bear those ills we have
+			Than fly to others that we know not of?
+			Thus conscience does make cowards of us all,
+			And thus the native hue of resolution
+			Is sicklied o'er with the pale cast of thought,
+			And enterprises of great pith and moment
+			With this regard their currents turn awry
+			And lose the name of action.- Soft you now!
+			The fair Ophelia!- Nymph, in thy orisons
+			Be all my sins rememb'red.
+		</p>
+		<cite>Hamlet, Act III, Scene 1</cite>
+
+
+
+
+	</body>
+</html>
